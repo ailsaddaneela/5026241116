d99c5e5159a450800e6e44cb1914639a0e39c9fc
ailsaddaneela <ailsadesdaneela68@gmail.com>
2026-04-26 20:46:31 +0700

    Tugas Pertemuan 7

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class DosenController extends Controller
 {
 
-    public function index(){
+public function index(){
     return "<h1>Halo ini adalah method index, dalam controller DosenController. - www.malasngoding.com</h1>";
 }
 
@@ -17,6 +17,42 @@ public function biodata(){
     $nama = "Ailsa Des Daneela" ;
     $pelajaran = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
     return view('biodata',['nama' => $nama , 'matkul' => $pelajaran]);
+}
+
+public function NRP(){
+    return view('5026241116');
+}
+
+public function contoh(){
+    return view('contoh');
+}
+
+public function intro(){
+    return view('intro');
+}
+
+public function news(){
+    return view('news');
+}
+
+public function news1(){
+    return view('news1');
+}
+
+public function responsive(){
+    return view('responsive');
+}
+
+public function template(){
+    return view('template');
+}
+
+public function index_pert5(){
+    return view('index_pert5');
+}
+
+public function linktree(){
+    return view('linktree');
 }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,14 @@ Route::get('pert5', function () {
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('5026241116', [DosenController::class, 'NRP']);
+Route::get('contoh', [DosenController::class, 'contoh']);
+Route::get('intro', [DosenController::class, 'intro']);
+Route::get('news', [DosenController::class, 'news']);
+Route::get('news1', [DosenController::class, 'news1']);
+Route::get('responsive', [DosenController::class, 'responsive']);
+Route::get('template', [DosenController::class, 'template']);
+
+Route::get('index_pert5', [DosenController::class, 'index_pert5']);
+Route::get('linktree', [DosenController::class, 'linktree']);
